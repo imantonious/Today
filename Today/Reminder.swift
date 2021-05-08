e95934369905abd509a8b67d9664f34c5a9b0225
Antonio Toriz <torizantonio15@gmail.com>
2021-05-07 23:59:40 -0700

    created Reminder struct

---
+++ Today/Reminder.swift
@@ -0,0 +1,17 @@
+//
+//  Reminder.swift
+//  Today
+//
+//  Created by Antonio Toriz on 07/05/2021.
+//
+
+import Foundation
+
+struct Reminder {
+    var title: String
+    var dueDate: Date
+    var notes: String? = nil
+    var isComplete: Bool = false
+}
+
+
