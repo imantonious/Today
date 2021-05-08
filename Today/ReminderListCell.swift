d2c9db1d5f1c7fc332696a409c464d81284defd9
Antonio Toriz <torizantonio15@gmail.com>
2021-05-08 01:07:23 -0700

    added done button action

--- Today/ReminderListCell.swift
+++ Today/ReminderListCell.swift
@@ -8,7 +8,16 @@
 import UIKit
 
 class ReminderListCell: UITableViewCell {
+    // Type aliases are helpful for referring to an existing type with a name that’s more expressive.
+    typealias DoneButtonAction = () -> Void
+    
     @IBOutlet var titleLabel: UILabel!
     @IBOutlet var dateLabel: UILabel!
     @IBOutlet var doneButton: UIButton!
+    
+    var doneButtonAction: DoneButtonAction?
+    
+    @IBAction func doneButtonTriggered(_ sender: UIButton ){
+        doneButtonAction?()
+    }
 }
