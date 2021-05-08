aaf6ba57af4f771b52bf5631306333d346521818
Antonio Toriz <torizantonio15@gmail.com>
2021-05-08 00:59:16 -0700

    created outlets

---
+++ Today/ReminderListCell.swift
@@ -0,0 +1,14 @@
+//
+//  ReminderListCell.swift
+//  Today
+//
+//  Created by Antonio Toriz on 08/05/2021.
+//
+
+import UIKit
+
+class ReminderListCell: UITableViewCell {
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var doneButton: UIButton!
+}
