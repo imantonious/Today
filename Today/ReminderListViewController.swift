3cc18231e3427e9dc057567c817dac2a9c5c0e96
Antonio Toriz <torizantonio15@gmail.com>
2021-05-08 19:05:57 -0700

    view now shows populated reminder list cells

--- Today/ReminderListViewController.swift
+++ Today/ReminderListViewController.swift
@@ -7,13 +7,26 @@
 
 import UIKit
 
-class ReminderListViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
+class ReminderListViewController: UITableViewController {
 
 }
 
+extension ReminderListViewController {
+    static let reminderListCellIdentifier = "ReminderListCell"
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Reminder.testData.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: Self.reminderListCellIdentifier, for: indexPath) as? ReminderListCell else {
+            fatalError("Unable to dequeue ReminderCell")
+        }
+        let reminder = Reminder.testData[indexPath.row]
+        // image constant for circle isComplete
+        let _ = reminder.isComplete ? UIImage(systemName: "circle.fill") : UIImage(systemName: "circle")
+        cell.titleLabel.text = reminder.title.description
+        cell.dateLabel.text = reminder.dueDate.description
+        return cell
+    }
+}
