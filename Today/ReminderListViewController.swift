41a92fd556ff2d918e944b6597379659d6b13516
Antonio Toriz <torizantonio15@gmail.com>
2021-05-20 22:54:10 -0700

    completed segue action and navigation

--- Today/ReminderListViewController.swift
+++ Today/ReminderListViewController.swift
@@ -8,7 +8,18 @@
 import UIKit
 
 class ReminderListViewController: UITableViewController {
-
+    static let showDetailSegueIdentifier = "ShowReminderDetailSegue"
+    
+    // This method notifies the view controller before a segue is performed.
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == Self.showDetailSegueIdentifier,
+           let destination = segue.destination as? ReminderDetailViewController,
+           let cell = sender as? UITableViewCell,
+           let indexPath = tableView.indexPath(for: cell) {
+            let reminder = Reminder.testData[indexPath.row]
+            destination.configure(with: reminder)
+        }
+    }
 }
 
 extension ReminderListViewController {
