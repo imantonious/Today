1794ca9e7bc8a4316ee564de08193c03dae0be26
Antonio Toriz <torizantonio15@gmail.com>
2021-05-22 10:57:13 -0700

    Extracted data source methods

--- Today/ReminderListDataSource.swift
+++ Today/ReminderListDataSource.swift
@@ -10,3 +10,25 @@ import UIKit
 class ReminderListDataSource: NSObject {
     
 }
+
+extension ReminderListDataSource: UITableViewDataSource {
+    static let reminderListCellIdentifier = "ReminderListCell"
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Reminder.testData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: Self.reminderListCellIdentifier, for: indexPath) as? ReminderListCell else {
+            fatalError("Unable to dequeue ReminderCell")
+        }
+        let reminder = Reminder.testData[indexPath.row]
+        cell.configure(title: reminder.title, doneText: reminder.dueDate.description, isDone: reminder.isComplete) {
+            // closure
+            Reminder.testData[indexPath.row].isComplete.toggle()
+            tableView.reloadRows(at: [indexPath], with: .none)
+        }
+        return cell
+    }
+
+}
