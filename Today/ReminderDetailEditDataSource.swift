c2c9694c8a7aabbb1ff56c298e54cf436d603f93
Antonio Toriz <torizantonio15@gmail.com>
2021-08-15 21:17:04 -0700

    created Edit Data Source

---
+++ Today/ReminderDetailEditDataSource.swift
@@ -0,0 +1,39 @@
+//
+//  ReminderDetailEditDataSource.swift
+//  Today
+//
+//  Created by Antonio Toriz on 8/15/21.
+//
+
+import UIKit
+
+class ReminderDetailEditDataSource: NSObject {
+    enum ReminderSection: Int, CaseIterable {
+        case title
+        case dueDate
+        case notes
+        
+        var displayText: String {
+            switch self {
+            case .title:
+                return "Title"
+            case .dueDate:
+                return "Date"
+            case .notes:
+                return "Notes"
+            }
+        }
+        
+        func cellIdentifier(for row: Int) -> String {
+            switch self {
+            case .title:
+                return "EditTitleCell"
+            case .dueDate:
+                return row == 0 ? "EditDateLabelCell" : "EditDateCell"
+            case .notes:
+                return "EditNotesCell"
+            }
+            
+        }
+    }
+}
