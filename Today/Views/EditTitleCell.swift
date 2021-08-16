f8602a25e2ef35cf71671bfe7eb8948a0acb1b7d
Antonio Toriz <torizantonio15@gmail.com>
2021-08-16 10:06:14 -0700

    edit title supported

--- Today/Views/EditTitleCell.swift
+++ Today/Views/EditTitleCell.swift
@@ -8,9 +8,29 @@
 import UIKit
 
 class EditTitleCell: UITableViewCell {
+    typealias TitleChangeAction = (String) -> Void
+    
+    private var titleChangeAction: TitleChangeAction?
+    
     @IBOutlet var titleTextField: UITextField!
     
-    func configure(title: String) {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        titleTextField.delegate = self
+    }
+    
+    func configure(title: String, changingAction: @escaping TitleChangeAction) {
         titleTextField.text = title
+        self.titleChangeAction = changingAction
+    }
+}
+
+extension EditTitleCell: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if let originalText = textField.text {
+            let title = (originalText as NSString).replacingCharacters(in: range, with: string)
+            titleChangeAction?(title)
+        }
+        return true
     }
 }
